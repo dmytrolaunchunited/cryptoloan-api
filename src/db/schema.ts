642eca0808d48e6e2fd1d24437e4f0c11f2949d6
Dmytro Paduchak <dmytro.paduchak@gmail.com>
2025-06-26 22:38:49 +0100

    fix

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -9,3 +9,14 @@ export const users = pgTable("users", {
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
   deletedAt: timestamp("deleted_at"),
 });
+
+// export const scoreSettings = pgTable("score_settings", {
+//   id: serial("id").primaryKey(),
+//   organization: varchar(),
+//   year: varchar(),
+//   email: varchar(),
+//   phone: varchar(),
+//   createdAt: timestamp("created_at").defaultNow().notNull(),
+//   updatedAt: timestamp("updated_at").defaultNow().notNull(),
+//   deletedAt: timestamp("deleted_at"),
+// });
